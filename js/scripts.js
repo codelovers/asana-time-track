f7a535744ba61dc3e54a64f8bd9fb45dbbfae7c5
Mathias Maier <maier@codelovers.de>
2012-07-10 19:28:25 +0200

    added basic js funktionality with ajax calling

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ $(function(){
     var apiKeyCookie = $.cookie('asana-api-key');
     var apiKeyForm = $('#api-key-form');
     var apiKeyFormSubmit = apiKeyForm.find('button[type=submit]');
-    var apiKeyEditDelete = apiKeyForm.find('#api-key-edit, #api-key-delete');
+    var apiKeyEditDelete = apiKeyForm.find('#api-key-edit, #api-key-delete, #api-key-refresh');
     var apiKeyInput = apiKeyForm.find('#api-key');
     var workspaceContainer = apiKeyForm.siblings('#workspace-container');
     var apiKeyImg = apiKeyForm.find('.ajax_img');
+    var workspaceLoader = $('.container').children('.ajax_img');
     $('#start-modal').modal();
     
     if(apiKeyCookie != null) {
@@ -13,6 +14,8 @@ $(function(){
         apiKeyEditDelete.show();
         apiKeyInput.attr('disabled', '').val(apiKeyCookie);
         workspaceContainer.fadeIn();
+        apiKeyImg.fadeIn();
+        workspacesAjaxCall();
     }
     
     $('#start-send').on('click', function(){
@@ -21,10 +24,27 @@ $(function(){
             apiKeyInput.attr('disabled', '');
             apiKeyImg.fadeIn();
             apiKeyFormSubmit.fadeOut();
-            workspaceContainer.delay('2000').fadeIn();
+            workspacesAjaxCall();
+            apiKeyEditDelete.show();
         }
         return false;
     });
+    
+    function workspacesAjaxCall() {
+        $.ajax({
+          type: "GET",
+          url: "request.php",
+          data: "apiKey=" + apiKeyInput.val(),
+          success: function( result ) {
+            workspaceContainer.html(result).fadeIn();
+            apiKeyImg.fadeOut();
+            },
+          error : function( msg ) {
+            workspaceContainer.html(msg.responseText).fadeIn();
+            apiKeyImg.fadeOut();
+            }
+        });
+    }
     
     $('#api-key-delete').on('click', function(){
         $.cookie('asana-api-key', null);
@@ -39,5 +59,59 @@ $(function(){
         apiKeyEditDelete.hide();
         workspaceContainer.hide();
         apiKeyInput.removeAttr('disabled').val(apiKeyCookie);
+        return false;
     });
+    
+    $('#api-key-refresh').on('click', function(){
+        workspaceContainer.hide();
+        apiKeyImg.fadeIn();
+        workspacesAjaxCall();
+        return false;
+    });
+    
+    $('#workspace-container').on('click', '#workspace-container > .workspace', function(){
+        var caption = $(this).find('h3').text();
+        var workspaceId = $(this).data('workspace-id');
+        $('#start-modal').modal('hide');
+        $('.workspace_caption').show().attr('data-workspace-id', workspaceId).html(caption);
+        projectsAjaxCall();
+        workspaceLoader.show();    });
+    
+    function projectsAjaxCall() {
+        $.ajax({
+          type: "GET",
+          url: "request.php",
+          data: "apiKey=" + apiKeyInput.val()+"&workspaceId=" + $('.workspace_caption').data('workspace-id'),
+          success: function( result ) {
+              $('#track-table').show().children('.nav-tabs').html(result);
+              var projectId = $('#track-table').children('.nav-tabs').children('.active').data('project-id');
+              tasksAjaxCall(projectId);
+            },
+          error : function( msg ) {
+              $('.container').html(msg.responseText).fadeIn();
+              workspaceLoader.fadeOut();
+            }
+        });
+    }
+    
+    $('.nav-tabs').on('click', '.nav-tabs > li:not(.active)', function(){        var projectId = $(this).data('project-id');
+        $('#tab1').hide();
+        workspaceLoader.show();
+        tasksAjaxCall(projectId);    });
+    
+    function tasksAjaxCall(projectId) {
+        $.ajax({
+          type: "GET",
+          url: "request.php",
+          data: "apiKey=" + apiKeyInput.val() + "&workspaceId=" + $('.workspace_caption').data('workspace-id') + "&projectId=" + projectId,
+          success: function( result ) {
+              $('#tab1').show().find('tbody').html(result);
+              workspaceLoader.fadeOut();
+            },
+          error : function( msg ) {
+              $('.container').append(msg.responseText).fadeIn();
+              workspaceLoader.fadeOut();
+            }
+        });
+    }
 });
---
+++ request.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * AsanaTimeTracking-Tool
+ * - track your time for every single task
+ *
+ * @author codelovers
+ * @author codelovers.de
+ * @version 1.0 (2012_07_07)
+ * @package asana_track_time
+ */
+
+require_once("AsanaApi.php");
+
+// get data
+$apiKey = $_GET['apiKey'];
+$workspaceId = $_GET['workspaceId'];
+$projectId = $_GET['projectId'];
+$updateId = $_GET['updateId'];
+
+// initalize
+$asana = new AsanaApi($apiKey); 
+$result = $asana->getWorkspaces();
+$userId = $asana->getUserId(); // should be saved in cookie
+
+// check if everything works fine
+if($asana->getResponseCode() == '200' && $result != '' ){
+	    
+    // workspaces
+    ////////////////////////////
+    if($workspaceId == '' && $projectId == ''){
+        
+       //echo '<b>Workspaces:</b><br />';
+
+    	$resultJson = json_decode($result);
+    	// $resultJson contains an object in json with all projects
+    	foreach($resultJson->data as $workspace){
+    		//echo '- <a href="?apiKey='.$apiKey.'&workspaceId=' . $workspace->id.'" target="_self">'. $workspace->name .'</a><br>';
+            echo '<div class="span4 well workspace" data-workspace-id="' . $workspace->id .'"><h3>' . $workspace->name .'</h3><p>Beschreibungs TEXT</p></div>';
+    	}
+    }
+
+
+    // projects
+    ////////////////////////////
+    if($workspaceId != '' && $projectId == ''){
+           
+       //echo '<b>Projects:</b><br />';
+       
+        $result = $asana->getProjects($workspaceId);
+        $result = json_decode($result);
+        $counter = 1;
+        foreach($result->data as $project){
+            //echo '- <a href="?apiKey='. $apiKey . '&workspaceId=' . $workspaceId.'&projectId=' . $project->id . '" target="_self">' . $project->name . '</a><br />';
+            $class = ($counter == 1) ? 'class="active"' : 'class=""';
+            echo '<li data-project-id="' . $project->id .'"' . $class .'><a href="#tab' . $counter . '" data-toggle="tab">' . $project->name . '</a></li>';            $counter++;
+        }
+    }
+    
+    // tasks
+    ////////////////////////////
+    if($projectId != ''){
+          
+        //echo '<b>Tasks:</b><br />';
+        
+        $result = $asana->getTasks($projectId, $workspaceId);
+        $result = json_decode($result);
+
+        foreach($result->data as $task){
+
+             $value = $asana->getGuessAndWorkedTime($task->name);
+             $taskState = $asana->getOneTask($task->id);
+
+             // task must be active and your own   
+             if($taskState['completed'] || $taskState['assignee'] != $userId) {
+                continue;
+             } else {
+                //echo '- <a href="?apiKey='. $apiKey . '&projectId=' . $projectId . '&updateId=' . $task->id . '" target="_self" data-guess-hours="'. $value['guessHours'] .'" data-guess-minutes="'. $value['guessMinutes'] .'" data-worked-hours="'. $value['workedHours'] .'" data-worked-minutes="'. $value['workedMinutes'] .'">' . $task->name . '</a><br />';
+                
+                echo '<tr data-task-id="' . $task->id . '">'
+                    .'<td>'. $task->name  .'</td>'
+                    .'<td data-guess-hours="'. $value['guessHours'] .'" data-guess-minutes="'. $value['guessMinutes'] .'"></td>'
+                    .'<td data-worked-hours="'. $value['workedHours'] .'" data-worked-minutes="'. $value['workedMinutes'] .'"></td>'
+                    .'<td><div class="progress progress-success progress-striped active">
+                            <div class="bar" style="width: 0%;"></div>
+                        </div>
+                      </td>'
+                    .'<td class="my_timer">
+                        <div class="time"></div>
+                        <button class="btn btn-success" type="submit">
+                            <i class="icon-white icon-play"></i> &nbsp; Start
+                        </button>
+                      </td>'
+                    .'</tr>';
+                 
+             }
+         }
+        
+    }
+    
+    // update
+    ////////////////////////////    
+    if($updateId != ''){
+        $asana->updateTask($updateId);
+    }
+    
+
+} else {
+    echo '<p>ERROR: Something went wrong! Maybe your asana api key does not fit.</p>';
+}
