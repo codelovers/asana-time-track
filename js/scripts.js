feccb60f778b66cda4ff035b50d845a7df8d97a9
Martin Kolb <martin.kolb@mkolb.de>
2012-07-26 10:08:52 +0200

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,9 @@ $(function(){
     var modalBackdrop = $('.modal-backdrop');
     var activeWorkspaceId = null;
 
-    $('.my_timer .start-stop').live('click', function(event){
+    $('.my_timer .btn').live('click', function(event){
         var locateClickedElement = $(this);
-        var locateAllButtons = $('.my_timer .start-stop');
+        var locateAllButtons = $('.my_timer .btn');
         
         // no more activity is possible, 
         // only one task can be active
